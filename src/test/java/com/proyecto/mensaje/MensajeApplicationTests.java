83c0f430ee0e9f6652eb7c81fcca0f901963eef7
diego <diegoonate3026@gmail.com>
2024-10-23 17:18:35 -0500

    empezando proyecto

---
+++ src/test/java/com/proyecto/mensaje/MensajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.mensaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensajeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
